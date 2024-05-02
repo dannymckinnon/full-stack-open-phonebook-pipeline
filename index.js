ea17ef56df18c6e4a5e86654b5e9c9ed64755d0c
Danny McKinnon <dannymckinnon.dm@gmail.com>
2024-05-02 14:21:42 -0700

    Enable actions on pull request

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post('/api/persons', (req, res, next) => {
 });
 
 app.get('/healthcheck', (req, res) => {
-  res.send('1'); // change this string to ensure a new version deployed
+  res.send('ok'); // change this string to ensure a new version deployed
 });
 
 app.get('/info', (req, res) => {
